ad8606f650dfb68597b50cdd3ea7a42044e1294c
Deepak Pai <paidpk07@gmail.com>
2020-10-12 22:14:13 +0530

    Committed build files

---
+++ public/precache-manifest.75f9955a700fa4e1c67f701cf79bda41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d79944ff61e4c7a8d2059a0c87daf9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6030dbbf94667ba14597",
+    "url": "/static/css/main.0dfff601.chunk.css"
+  },
+  {
+    "revision": "6a0c34abdaba4391b3bf",
+    "url": "/static/js/2.80ecc4f6.chunk.js"
+  },
+  {
+    "revision": "19acd380d13f406f4aea30b4f3c1897a",
+    "url": "/static/js/2.80ecc4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6030dbbf94667ba14597",
+    "url": "/static/js/main.3528ac53.chunk.js"
+  },
+  {
+    "revision": "4847491b8a758f21eb62",
+    "url": "/static/js/runtime-main.6fc34de3.js"
+  }
+]);
